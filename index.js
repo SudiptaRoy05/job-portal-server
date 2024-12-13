2e9e52c37177ac520ed8fbddc0d0f3644878efff
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-12-13 16:44:02 +0600

    get data through specific id api added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lue0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,8 +23,22 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-        // const database = client.db("jobApplications");
-        // const jobCollection = database.collection("jobCollection");
+        const database = client.db("job-application");
+        const jobCollection = database.collection("jobCollection");
+
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await jobCollection.findOne(query);
+            res.send(result);
+        })
 
         // app.post('jobs/',async(req, res)=>{
         //     const 
